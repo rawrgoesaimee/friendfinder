59286d438db510a9b758f7efb80b674d96506d69
rawrgoesaimee <rawrgoesaimee@hotmail.com>
2019-10-06 15:42:39 -0700

    adding code to html and js

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+let friends = [
+	{
+		name: "Steph",
+		photo: "https://sportshub.cbsistatic.com/i/r/2018/12/17/3d6319cc-1340-486d-bbc1-48a99b3923e7/thumbnail/1200x675/789f22e2bcf6c43e36a758763020ab39/stephen-curry.jpg",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Draymond",
+		photo: "https://www.mercurynews.com/wp-content/uploads/2018/09/BNG-L-WARRIORS-0925-101.jpg?w=525",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Klay",
+		photo: "https://amp.businessinsider.com/images/5bd88050f556fc17340ff441-750-375.jpg",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	}
+];
+
+module.exports = friends;
